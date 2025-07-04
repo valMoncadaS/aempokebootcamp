4e400b70ef61d342306bee1079d8c13839964cf0
jose <josemicrobiologo@gmail.com>
2025-07-04 16:08:26 -0500

    Creates PokemonCardsImpl

---
+++ core/src/main/java/com/aem/pokebootcamp/core/models/impl/PokemonCardsImpl.java
@@ -0,0 +1,20 @@
+package com.aem.pokebootcamp.core.models.impl;
+
+import com.aem.pokebootcamp.core.models.PokemonCards;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+
+import lombok.Getter;
+import java.util.List;
+
+@Model(adaptables = SlingHttpServletRequest.class, adapters = {PokemonCards.class}, resourceType = {PokemonCardsImpl.RESOURCE}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+
+public class PokemonCardsImpl implements PokemonCards {
+    public static final String RESOURCE = "aempokebootcamp/components/pokemoncards";
+
+    @Getter
+    @ChildResource
+    private List<String> pokemonCards;
+}
