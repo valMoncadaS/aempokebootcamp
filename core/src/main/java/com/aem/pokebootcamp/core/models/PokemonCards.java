a4e34d0752bb5eaa88fafac694c39f36393c7cfa
jose <josemicrobiologo@gmail.com>
2025-07-04 15:57:14 -0500

    Creates PokemonCards model

---
+++ core/src/main/java/com/aem/pokebootcamp/core/models/PokemonCards.java
@@ -0,0 +1,8 @@
+package com.aem.pokebootcamp.core.models;
+import java.util.List;
+import com.aem.pokebootcamp.core.models.impl.PokemonItem;
+
+public interface PokemonCards {
+    List<PokemonItem> getPokemonCards();
+}
+
