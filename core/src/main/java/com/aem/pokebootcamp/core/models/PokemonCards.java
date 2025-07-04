14f9f644d008dc59102723858605696dc1005343
jose <josemicrobiologo@gmail.com>
2025-07-04 16:09:22 -0500

    Adds some spaces

--- core/src/main/java/com/aem/pokebootcamp/core/models/PokemonCards.java
+++ core/src/main/java/com/aem/pokebootcamp/core/models/PokemonCards.java
@@ -1,8 +1,10 @@
 package com.aem.pokebootcamp.core.models;
+
 import java.util.List;
 import com.aem.pokebootcamp.core.models.impl.PokemonItem;
 
 public interface PokemonCards {
+
     List<PokemonItem> getPokemonCards();
 }
 
