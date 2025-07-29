b11277130c0d1033bd1d109c5b2e98499727e0d0
jose <josemicrobiologo@gmail.com>
2025-07-28 23:16:36 -0500

    Creates pokemon stats interface

---
+++ core/src/main/java/com/aem/pokebootcamp/core/models/PokemonStats.java
@@ -0,0 +1,10 @@
+package com.aem.pokebootcamp.core.models;
+
+public interface PokemonStats {
+    int getHp();
+    int getAttack();
+    int getDefense();
+    int getSpecialAttack();
+    int getSpecialDefense();
+    int getSpeed();
+}
