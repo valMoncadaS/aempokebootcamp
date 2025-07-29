d22969b4380ad605354e31c0f25f02dc6e28cd9f
jose <josemicrobiologo@gmail.com>
2025-07-28 23:17:04 -0500

    Implements pokemon stats

--- core/src/main/java/com/aem/pokebootcamp/core/models/impl/PokemonStatsImpl.java
+++ core/src/main/java/com/aem/pokebootcamp/core/models/impl/PokemonStatsImpl.java
@@ -5,31 +5,38 @@ import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import com.aem.pokebootcamp.core.models.PokemonStats;
 
 import javax.inject.Inject;
 
-@Getter
 @Model(
     adaptables = Resource.class,
+    adapters = PokemonStats.class,
     defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
-public class PokemonStatsImpl {
+public class PokemonStatsImpl  implements PokemonStats {
 
+    @Getter
     @ValueMapValue
     private int hp;
 
+    @Getter
     @ValueMapValue
     private int attack;
 
+    @Getter
     @ValueMapValue
     private int defense;
 
+    @Getter
     @ValueMapValue
     private int specialAttack;
 
+    @Getter
     @ValueMapValue
     private int specialDefense;
 
+    @Getter
     @ValueMapValue
     private int speed;
 }
