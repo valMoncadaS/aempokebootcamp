73f85e7118089a3329ec740afa82dff49d3ef0d0
alejandro.cifuentes <alejandro.cifuentes@dish.com>
2025-09-14 13:56:31 -0500

    AEMPOKEBOOTCAMP-11: Refactor type handling to use TagXFService in PokemonCard and show XFs

--- core/src/test/java/com/aem/pokebootcamp/core/bean/PokemonCardBeanTest.java
+++ core/src/test/java/com/aem/pokebootcamp/core/bean/PokemonCardBeanTest.java
@@ -1,5 +1,6 @@
 package com.aem.pokebootcamp.core.bean;
 
+import com.aem.pokebootcamp.core.services.TagXFService;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 import org.apache.sling.api.resource.Resource;
@@ -12,6 +13,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Unit tests for the {@link PokemonCard} class.
@@ -24,11 +27,16 @@ class PokemonCardBeanTest {
 
     private final AemContext context = new AemContext();
 
+    private TagXFService tagXFService;
+
+
     @BeforeEach
     void setUp() {
         // Load any common test resources if needed
         context.load().json("/com/aem/pokebootcamp/core/beans/PokemonCardBeanTest.json",
                 "/content");
+        tagXFService = mock(TagXFService.class);
+        context.registerService(TagXFService.class, tagXFService);
     }
 
     @Test
@@ -45,7 +53,16 @@ class PokemonCardBeanTest {
         assertEquals("/content/dam/aempokebootcamp/pokemon-images/001.png", pokemonCard.getPokemonImage(),
                 "Expected Pokemon image to be '/content/dam/aempokebootcamp/pokemon-images/001.png'");
 
-        final List<String> expectedTypes = Arrays.asList("Grass", "Poison");
+        final List<String> expectedTypes = Arrays.asList("pokemon-types:grass", "pokemon-types:poison");
         assertEquals(expectedTypes, pokemonCard.getType(), "Expected Pokemon types to be 'Grass, Poison'");
+
+
+        when(tagXFService.getXFsByTags(List.of("pokemon-types:grass", "pokemon-types:poison")))
+                .thenReturn(List.of("/content/experience-fragments/aempokebootcamp/us/en/site/type/grass",
+                        "/content/experience-fragments/aempokebootcamp/us/en/site/type/poison"));
+        assertEquals("/content/experience-fragments/aempokebootcamp/us/en/site/type/grass",
+                pokemonCard.getTypes().get(0), "Expected Pokemon type to be Grass XF path");
+        assertEquals("/content/experience-fragments/aempokebootcamp/us/en/site/type/poison",
+                pokemonCard.getTypes().get(1), "Expected Pokemon type to be Poison XF path");
     }
 }
