488583c504de88b4788bba8bf925dd2c394c88b9
alejandro.cifuentes <alejandro.cifuentes@dish.com>
2025-09-17 10:52:42 -0500

    AEMPOKEBOOTCAMP-15: Added PokemonDTO

---
+++ core/src/main/java/com/aem/pokebootcamp/core/dto/PokemonDTO.java
@@ -0,0 +1,51 @@
+package com.aem.pokebootcamp.core.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Represents a Data Transfer Object (DTO) for a Pokemon.
+ * Provides encapsulation of various Pokemon attributes that include general characteristics,
+ */
+@Builder
+public class PokemonDTO {
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private List<String> types;
+
+    @Getter
+    @Setter
+    private List<String> weakness;
+
+    @Getter
+    @Setter
+    private String height;
+
+    @Getter
+    @Setter
+    private String weight;
+
+    @Getter
+    @Setter
+    private String category;
+
+    @Getter
+    @Setter
+    private String ability;
+
+    @Getter
+    @Setter
+    private List<String> gender;
+
+    @Getter
+    @Setter
+    private Map<String, Integer> stats;
+}
